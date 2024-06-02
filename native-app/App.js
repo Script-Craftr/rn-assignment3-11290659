1526a25a6dc14fd4f4e98932afbe5110cbbcb26c
Script-Craftr <ennbadger@st.ug.edu.gh>
2024-06-02 01:55:36 -1200

    saved final files and images

--- native-app/App.js
+++ native-app/App.js
@@ -3,16 +3,18 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import SearchBar from './projectComponents/SearchBar';
 import Frame1 from './projectComponents/Frame1';
 import OngoingTask from './projectComponents/OngoingTask';
+import CategorySection from './projectComponents/CategorySection';
 
 export default function App() {
   return (
     <ScrollView>
       <View style={styles.container}>
         <Text>Hello Annoying React Native</Text>
-        <Frame1 />
-        <SearchBar />
-        <OngoingTask />
-    </View>
+        <Frame1/>
+        <SearchBar/>
+        <CategorySection/>
+        <OngoingTask/>
+      </View>
     </ScrollView>
     
   );
